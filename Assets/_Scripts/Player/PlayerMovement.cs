d95420adec8bbda4ec0e016a4a85c7ed894be174
zwazoman <gloupik.lemaire@gmail.com>
2024-08-25 20:58:04 +0200

    player good

--- Assets/_Scripts/Player/PlayerMovement.cs
+++ Assets/_Scripts/Player/PlayerMovement.cs
@@ -3,28 +3,44 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
-public class PlayerMove : MonoBehaviour
+public class PlayerMovement : MonoBehaviour
 {
     [Header("References")]
     [SerializeField] CharacterController controller;
+    [SerializeField] Transform _groundCheck;
 
     [Header("Parameters")]
     [SerializeField] float _playerSpeed = 10f;
-    [SerializeField] float _sprintMultiplierValue;
+    [SerializeField] float _sprintMultiplierValue = 2f;
+    [SerializeField] float _groundDistance = 0.4f;
+    [SerializeField] LayerMask _groundMask;
+    [SerializeField] float _gravityMultiplier = 2f;
+
+    [HideInInspector] public float Gravity = -9.81f;
+    [HideInInspector] public bool IsGrounded;
 
     float _sprintMultiplier = 1;
     float x;
     float z;
 
-    void Update()
+    Vector3 velocity;
+
+    private void Awake()
     {
-        Move();
+        Gravity = Gravity * _gravityMultiplier;
     }
 
-    void Move()
+    void Update()
     {
+        print(velocity);
+        IsGrounded = Physics.CheckSphere(_groundCheck.position, _groundDistance, _groundMask);
+        if (IsGrounded && velocity.y < 0) velocity.y = -2f;
+
         Vector3 move = transform.right * x + transform.forward * z;
         controller.Move(move * _playerSpeed * _sprintMultiplier * Time.deltaTime);
+
+        velocity.y += Gravity * Time.deltaTime;
+        controller.Move(velocity * Time.deltaTime);
     }
 
     public void OnMove(InputAction.CallbackContext context)
